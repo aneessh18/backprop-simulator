1d0285f7189a50e2f65627af058ca7c15516e0b6
indavarapuaneesh <indavarapuaneesh@gmail.com>
2021-01-09 17:44:59 +0530

    add Input component

---
+++ src/Input.js
@@ -0,0 +1,38 @@
+export default function Input(props)
+{
+  return (
+    <div>
+      <text>{props.name} </text>
+      <input type={props.type} value={props.value===0 ? '':props.value} size={props.hasOwnProperty('size')? props.size : 20}
+      onChange={e => {
+          if(props.type === "number")
+          {
+              let num = parseFloat(e.target.value);
+            if(isNaN(num))
+            num = 0;
+            
+            props.setInput(num)
+          }
+          else if(props.parameter === 'arch')
+          {
+            let arch = e.target.value.split(":");
+            props.setInput(arch);
+          }
+          else if(props.parameter === 'trainData')
+          {
+            let trainData = e.target.value.split(' ');
+            console.log(trainData);
+            let buf = []
+            for(let i=0;i<trainData.length;i++)
+            {
+              buf.push(trainData[i].slice(1, trainData[i].length-1).split(',').map(s => parseInt(s)));
+            }
+            // console.log(buf);
+            props.setInput(buf);
+          }
+        }
+      }></input>
+    </div>
+  )
+}
+
