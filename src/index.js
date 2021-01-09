00e5bc501387768b1bbccce9757ec2ba7140906a
indavarapuaneesh <indavarapuaneesh@gmail.com>
2021-01-09 17:45:25 +0530

    add react router for multiple simulators

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Backprop from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Adaline from './Adaline'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+function Main()
+{
+  return (
+    <Router>
+      <div>
+        <nav>
+          <li>
+            <Link to="/backprop">Backprop Simulator</Link>
+          </li>
+          <li>
+            <Link to="/adaline">Adaline Simulator</Link>
+          </li>
+        </nav>
+        <Switch>
+          <Route path="/backprop">
+          <Backprop initialWeight={0.2} arch={[2,2,2,1]} epochs={1} learningRate={0.2} trainData = {[[0,0,1],[0,1,0],[1,0,0],[1,1,1]]} />
+          </Route>
+          <Route path="/adaline">
+          <Adaline initialWeight={0.2} epochs={1} learningRate={0.2} trainData = {[[0,0,0],[0,1,0],[1,0,0],[1,1,1]]}/>
+          </Route>
+        </Switch>
+      </div>   
+    </Router>
+  )
+}
 ReactDOM.render(
   <React.StrictMode>
-    <Backprop initialWeight={0.2} arch={[2,2,2,1]} epochs={1} learningRate={0.2} trainData = {[[0,0,1],[0,1,0],[1,0,0],[1,1,1]]} />
+    <Main/>
   </React.StrictMode>,
   document.getElementById('root')
 );
