f40d18c68493c673fc8a93e097a267f84dd0de9c
indavarapuaneesh <indavarapuaneesh@gmail.com>
2021-01-12 18:26:28 +0530

    add momentum

--- src/Adaline.js
+++ src/Adaline.js
@@ -3,6 +3,6 @@ import Train from "./Utilities";
 export default function Adaline(props)
 {
     return(
-        <Train initialWeight={0.2} epochs={1} learningRate={0.2} trainData = {[[0,0,0],[0,1,0],[1,0,0],[1,1,1]]} type="adaline"/>
+        <Train initialWeight={0.2} epochs={1} learningRate={0.2} trainData = {[[0,0,0],[0,1,0],[1,0,0],[1,1,1]]} type="adaline" momentum={0}/>
     )
 }
