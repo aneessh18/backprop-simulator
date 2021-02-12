40858f36b2806e321c3036d577833d405fbd0561
indavarapuaneesh <indavarapuaneesh@gmail.com>
2021-02-12 20:32:02 +0530

    fix ===

--- src/components/Corpus.js
+++ src/components/Corpus.js
@@ -16,7 +16,7 @@ function indianCurrency(num)
     x=x.toString();
     var lastThree = x.substring(x.length-3);
     var otherNumbers = x.substring(0,x.length-3);
-    if(otherNumbers != '')
+    if(otherNumbers !== '')
         lastThree = ',' + lastThree;
     var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
     return res
